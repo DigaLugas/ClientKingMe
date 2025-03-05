7b7ecc016b492768f445764eaee14982678d6514
DigaLugas <lucasgustavorodrigues10@gmail.com>
2025-03-04 22:35:52 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ClientKingMe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxPartidas = new System.Windows.Forms.ComboBox();
+            this.detalhesPartida = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nomePartida = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.senhaPartida = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Selecionar Partida:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBoxPartidas
+            // 
+            this.comboBoxPartidas.FormattingEnabled = true;
+            this.comboBoxPartidas.Location = new System.Drawing.Point(12, 63);
+            this.comboBoxPartidas.Name = "comboBoxPartidas";
+            this.comboBoxPartidas.Size = new System.Drawing.Size(284, 21);
+            this.comboBoxPartidas.TabIndex = 3;
+            this.comboBoxPartidas.SelectedIndexChanged += new System.EventHandler(this.comboBoxPartidas_SelectedIndexChanged);
+            // 
+            // detalhesPartida
+            // 
+            this.detalhesPartida.Enabled = false;
+            this.detalhesPartida.Location = new System.Drawing.Point(12, 108);
+            this.detalhesPartida.Multiline = true;
+            this.detalhesPartida.Name = "detalhesPartida";
+            this.detalhesPartida.Size = new System.Drawing.Size(284, 320);
+            this.detalhesPartida.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(326, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 24);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Criar Partida:";
+            this.label7.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(639, -1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Monges de Clunny";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(740, -1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "DLL: 1.1";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // nomePartida
+            // 
+            this.nomePartida.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.nomePartida.Location = new System.Drawing.Point(330, 136);
+            this.nomePartida.Name = "nomePartida";
+            this.nomePartida.Size = new System.Drawing.Size(150, 23);
+            this.nomePartida.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label4.Location = new System.Drawing.Point(330, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Nome:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label5.Location = new System.Drawing.Point(330, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Senha:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(324, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // senhaPartida
+            // 
+            this.senhaPartida.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.senhaPartida.Location = new System.Drawing.Point(330, 201);
+            this.senhaPartida.Name = "senhaPartida";
+            this.senhaPartida.Size = new System.Drawing.Size(150, 23);
+            this.senhaPartida.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(330, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 43);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Confirmar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.senhaPartida);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nomePartida);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.detalhesPartida);
+            this.Controls.Add(this.comboBoxPartidas);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxPartidas;
+        private System.Windows.Forms.TextBox detalhesPartida;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nomePartida;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox senhaPartida;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KingMeServer;
+
+namespace ClientKingMe
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            ApplyCustomStyling();
+            atulizarComboBox();
+        }
+
+        private void ApplyCustomStyling()
+        {
+            // Definir tema de cores
+            Color primaryColor = Color.FromArgb(57, 89, 156);      
+            Color secondaryColor = Color.FromArgb(241, 245, 249); 
+            Color accentColor = Color.FromArgb(34, 197, 94);      
+
+            // Estilo do formulário
+            this.BackColor = secondaryColor;
+            this.Font = new Font("Segoe UI", 9, FontStyle.Regular);
+            this.Text = "KingMe - Gerenciador de Partidas";
+
+            // Estilização dos controles
+            StyleComboBox(comboBoxPartidas, primaryColor);
+            StyleTextBox(nomePartida, primaryColor);
+            StyleTextBox(senhaPartida, primaryColor);
+            StyleTextBox(detalhesPartida, primaryColor);
+            StyleButton(button1, primaryColor, accentColor);
+
+            // Labels com estilo moderno
+            StyleLabel(label1, primaryColor);
+            StyleLabel(label2, primaryColor);
+            StyleLabel(label3, primaryColor);
+            StyleLabel(label4, primaryColor);
+            StyleLabel(label5, primaryColor);
+            StyleLabel(label6, primaryColor);
+            StyleLabel(label7, primaryColor);
+
+
+
+            // Adicionar sombra sutil
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+        }
+
+        private void StyleComboBox(ComboBox comboBox, Color primaryColor)
+        {
+            comboBox.BackColor = Color.White;
+            comboBox.ForeColor = primaryColor;
+            comboBox.Font = new Font("Segoe UI", 10);
+            comboBox.FlatStyle = FlatStyle.Flat;
+            comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void StyleTextBox(TextBox textBox, Color primaryColor)
+        {
+            textBox.BackColor = Color.White;
+            textBox.ForeColor = primaryColor;
+            textBox.Font = new Font("Segoe UI", 10);
+            textBox.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        private void StyleButton(Button button, Color primaryColor, Color accentColor)
+        {
+            button.BackColor = accentColor;
+            button.ForeColor = Color.White;
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderColor = primaryColor;
+            button.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            button.Cursor = Cursors.Hand;
+        }
+
+        private void StyleLabel(Label label, Color primaryColor)
+        {
+            label.ForeColor = primaryColor;
+            label.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void comboBoxPartidas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxPartidas.SelectedIndex >= 0)
+            {
+
+                detalhesPartida.Text = "";
+
+  
+                string linha = comboBoxPartidas.SelectedItem.ToString();
+                string[] detalhes = linha.Split('|');
+                string id = detalhes[0].Split(':')[1].Trim();
+                detalhesPartida.Text += "--- DETALHES DA PARTIDA ---" + Environment.NewLine;
+                detalhesPartida.Text += linha + Environment.NewLine;
+                detalhesPartida.Text += Environment.NewLine;
+
+    
+                string retorno = Jogo.ListarJogadores(Convert.ToInt32(id));
+                string[] jogadores = retorno.Split('\n');
+
+                detalhesPartida.Text += "--- JOGADORES ---" + Environment.NewLine;
+                foreach (var jogador in jogadores)
+                {
+                    if (!string.IsNullOrWhiteSpace(jogador))
+                    {
+                        string[] detalhesJogador = jogador.Split(',');
+                  
+                        string linhaFormatada = $"ID: {detalhesJogador[0]} | Nome: {detalhesJogador[1]} | Pontuação: {detalhesJogador[2]}";
+                        detalhesPartida.Text += linhaFormatada + Environment.NewLine;
+                    }
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           string retorno = Jogo.CriarPartida(nomePartida.Text, senhaPartida.Text, Constants.NomeDoGrupo);
+            if (!retorno.Contains("ERRO"))
+            {
+                MessageBox.Show($"Partida criada com sucesso!\n {retorno}");
+            }
+            else
+            {
+                MessageBox.Show($"{retorno}");
+            }
+            atulizarComboBox();
+        }
+        private void atulizarComboBox()
+        {
+            comboBoxPartidas.Items.Clear();
+
+            string retorno = Jogo.ListarPartidas("T");
+
+            string[] partidas = retorno.Split('\n');
+
+            foreach (string partida in partidas)
+            {
+                if (!string.IsNullOrWhiteSpace(partida))
+                {
+                    string[] detalhes = partida.Split(',');
+
+                    string linhaFormatada = $"ID: {detalhes[0]} | Nome: {detalhes[1]} | Data: {detalhes[2]} | Status: {detalhes[3]}";
+
+                    comboBoxPartidas.Items.Add(linhaFormatada);
+                }
+            }
+            if (comboBoxPartidas.Items.Count > 0)
+            {
+                comboBoxPartidas.SelectedIndex = 0;
+            }
+        }
+    }
+}
